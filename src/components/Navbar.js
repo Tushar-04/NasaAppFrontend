6b92b4023492098f8c71bf98e7f6f1d6384d6922
Tushar <s.v.tushar04@gmail.com>
2022-10-02 13:29:53 +0530

    error solve

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,12 +20,12 @@ const Navbar = () => {
 
   return (
     <div className={color ? "header header-bg" : "header"}>
-      <Link to="/">
+      <Link to="/NasaAppFrontend">
         <h1>Asteroids</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/NasaAppFrontend">Home</Link>
         </li>
         <li>
           <Link to="/pricing">All Resources</Link>
